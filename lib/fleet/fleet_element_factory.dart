4cef6125a5bfd244db47640729b3508435265bfb
aridai <aridai.net@outlook.jp>
2021-09-20 16:59:42 +0900

    Fleet要素のIDの採番方式を変更

--- lib/fleet/fleet_element_factory.dart
+++ lib/fleet/fleet_element_factory.dart
@@ -9,13 +9,9 @@ class FleetElementFactory {
   static const _defaultScale = 1.0;
   static const _defaultAngle = 0.0;
 
-  //  カウンタ
-  //  (連番IDを発行するため。)
-  int _count = 0;
-
   /// テキスト要素を生成する。
   FleetTextElement createText(String text) {
-    final id = _count++;
+    final id = _generateId();
     final element = FleetElement.text(
       id,
       text,
@@ -29,7 +25,7 @@ class FleetElementFactory {
 
   /// 絵文字要素を生成する。
   FleetEmojiElement createEmoji(String emoji) {
-    final id = _count++;
+    final id = _generateId();
     final element = FleetElement.emoji(
       id,
       emoji,
@@ -43,7 +39,7 @@ class FleetElementFactory {
 
   /// 画像要素を生成する。
   FleetImageElement createImage(String fileName, Uint8List imageBytes) {
-    final id = _count++;
+    final id = _generateId();
     final element = FleetElement.image(
       id,
       fileName,
@@ -55,4 +51,7 @@ class FleetElementFactory {
 
     return element as FleetImageElement;
   }
+
+  //  IDを生成する。
+  int _generateId() => DateTime.now().microsecondsSinceEpoch;
 }
